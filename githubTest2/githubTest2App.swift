68cbf349eb50005d5feaac53ed341fbaa3dd4721
jkwon-dev <kwonejk@gmail.com>
2023-10-09 09:20:19 +0800

    Initial Commit

---
+++ githubTest2/githubTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  githubTest2App.swift
+//  githubTest2
+//
+//  Created by kwon eunji on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct githubTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
